25b52eb82a83151978030778e32d0191946823b6
Fraz <11639362+Frazurbluu@users.noreply.github.com>
2020-05-12 00:12:25 -0400

    Minor fixes (#131)
    * Minor Fixes
    
    * .

--- Scripts/Expansion/TOL/Items/MyrmidexInvasion/BattleSpawner.cs
+++ Scripts/Expansion/TOL/Items/MyrmidexInvasion/BattleSpawner.cs
@@ -1006,8 +1006,10 @@ namespace Server.Engines.MyrmidexInvasion
             bflag = new BattleFlag(0x42C, 2520);
             bflag.MoveToWorld(new Point3D(914, 1793, 6), Map.TerMur);
 
-            bflag = new BattleFlag(0x42D, 2520);
-            bflag.MoveToWorld(new Point3D(913, 1793, 6), Map.TerMur);
+            st = new Static(0x42D);
+            st.MoveToWorld(new Point3D(913, 1793, 6), Map.TerMur);
+            //bflag = new BattleFlag(0x42D, 2520);
+            //bflag.MoveToWorld(new Point3D(913, 1793, 6), Map.TerMur);
         }
     }
 }
